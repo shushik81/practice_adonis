167eb6be4d777c8c099abb4079987d084c3b31a2
kulykaldr <kulyk.aldr@gmail.com>
2019-01-02 13:24:16 -0800

    Models, Migrations, Seeds

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -1,25 +1,17 @@
+const Product = use('App/Models/Product');
+
 class ProductController {
   /**
    * Show a list of all products.
    * GET products
    *
-   * @param {object} ctx
-   * @param {Response} ctx.response
    */
-  async index({ response }) {
-    return response
-      .status(200)
-      .json([
-        { id: 1, product: 'Nokia 5230', type: 'mobile phone', price: '₴190' },
-        { id: 2, product: 'Nokia 5800', type: 'mobile phone', price: '₴290' },
-        { id: 3, product: 'Canon MP230', type: 'printer', price: '₴790' }
-      ]);
+  async index() {
+    return Product.all();
   }
 
   /**
    * Show all product types
-   * @param request
-   * @param response
    */
   async showTypes({ response }) {
     return response.status(201).json(['mobile phone', 'notebook', 'printer', 'televisor']);
@@ -28,10 +20,6 @@ class ProductController {
   /**
    * Create/save a new product.
    * POST products
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
   async store({ request, response }) {
     return response.status(201).json(request.all());
@@ -40,14 +28,11 @@ class ProductController {
   /**
    * Display a single product.
    * GET products/:id
-   *
-   * @param {object} ctx
-   * @param {Response} ctx.response
    */
   async show({ response, params: { id } }) {
     if (id === '1') {
       return response.status(200).json({
-        id,
+        product_id: id,
         product: 'Nokia 5230',
         type: 'mobile phone',
         price: '₴190'
@@ -60,9 +45,6 @@ class ProductController {
   /**
    * Update product details.
    * PUT or PATCH products/:id
-   *
-   * @param {object} ctx
-   * @param {Response} ctx.response
    */
   async update({ response, params: { id } }) {
     return response.status(202).json({ message: `Product id${id} updated` });
@@ -71,9 +53,6 @@ class ProductController {
   /**
    * Delete a product with id.
    * DELETE products/:id
-   *
-   * @param {object} ctx
-   * @param {Response} ctx.response
    */
   async destroy({ response, params: { id } }) {
     return response.status(205).json({ message: `Product id${id} deleted` });
--- start/routes.js
+++ start/routes.js
@@ -1,24 +1,10 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route');
 
 Route.get('/', () => ({ status: 'Ok', version: '1.0.0' }));
-Route.post('/login', 'AuthController.login'); // okay
-Route.post('/products/add', 'ProductController.store'); //
-Route.get('/products', 'ProductController.index'); // okay
-Route.get('/products/types', 'ProductController.showTypes'); // okay
-Route.get('/products/:id', 'ProductController.show'); // okay
+Route.post('/login', 'AuthController.login');
+Route.post('/products/add', 'ProductController.store');
+Route.get('/products', 'ProductController.index');
+Route.get('/products/types', 'ProductController.showTypes');
+Route.get('/products/:id', 'ProductController.show');
 Route.patch('/products/:id', 'ProductController.update');
 Route.delete('/products/:id', 'ProductController.destroy');
