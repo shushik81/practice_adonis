444915f21fae53a74ac47b1a6082a8d7d5e91574
kulykaldr <kulyk.aldr@gmail.com>
2019-01-03 03:51:44 -0800

    fix routes

--- start/routes.js
+++ start/routes.js
@@ -8,18 +8,18 @@ Route.post('/products', 'ProductController.store');
 Route.get('/products/:id', 'ProductController.show');
 Route.put('/products/:id', 'ProductController.update');
 Route.patch('/products/:id', 'ProductController.update');
-Route.delete('/products/:id', 'ProductController.destroy')
+Route.delete('/products/:id', 'ProductController.destroy');
 
 Route.get('/types', 'TypeController.index');
 Route.post('/types', 'TypeController.store');
 Route.get('/types/:id', 'TypeController.show');
 Route.put('/types/:id', 'TypeController.update');
 Route.patch('/types/:id', 'TypeController.update');
-Route.delete('/types/:id', 'TypeController.destroy')
+Route.delete('/types/:id', 'TypeController.destroy');
 
-Route.get('/types/:id/attrs', 'AttrController.index');
-Route.post('/types/:id/attrs', 'AttrController.store');
-Route.get('/types/:id/attrs/:id', 'AttrController.show');
-Route.put('/types/:id/attrs/:id', 'AttrController.update');
-Route.patch('/types/:id/attrs/:id', 'AttrController.update');
-Route.delete('/types/:id/attrs/:id', 'AttrController.destoy');
+Route.get('/types/:type/attrs', 'AttrController.index');
+Route.post('/types/:type/attrs', 'AttrController.store');
+Route.get('/types/:type/attrs/:id', 'AttrController.show');
+Route.put('/types/:type/attrs/:id', 'AttrController.update');
+Route.patch('/types/:type/attrs/:id', 'AttrController.update');
+Route.delete('/types/:type/attrs/:id', 'AttrController.destroy');
